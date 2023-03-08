bd705cfd16a2501b6cde95af02ba80c06ae1fd35
highlander santos <highlander@lanlink.com.br>
2023-03-08 16:02:20 -0300

    feat: add core components

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,26 @@
+import { Avatar, AvatarProps } from '@rani-ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/highlander08.png',
+    alt: 'Ranielli Montagna',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
+}
---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,13 @@
+# @rani-ignite-ui/react
+
+## 2.0.1
+
+### Patch Changes
+
+- Adiciona alguns ajustes para futuras features
+
+## 2.0.0
+
+### Major Changes
+
+- Publish package for first time
